38687f412e1310c9a010b4f18e7d7ed96fa3aedf
YE-EUN <musiclove529@naver.com>
2021-06-22 23:52:41 +0900

    add .java

---
+++ JAVA/p10870.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class p10870 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		bw.write(String.valueOf(fibonacci(n)));
+		bw.flush();
+	}
+	public static int fibonacci(int n) {
+		if(n<=1) {
+			return n;
+		}else {
+			return (fibonacci(n-1) + fibonacci(n-2));
+		}
+	}
+
+}
