523812d42297bb886c88ca02f7d2a7f087b9e0a2
YE-EUN <musiclove529@naver.com>
2021-07-30 00:08:34 +0900

    2021-07-30 java baekjoon

---
+++ JAVA/p1550.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class p1550 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int i = Integer.parseInt(br.readLine(),16);
+		System.out.println(i);
+
+	}
+
+}
