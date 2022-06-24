41faade28437fd42565923d6d720ecfdfb6c4022
YE-EUN <musiclove529@naver.com>
2022-06-24 12:19:18 +0900

    p16236

--- JAVA/baekjoon/src/p16236.java
+++ JAVA/baekjoon/src/p16236.java
@@ -1,90 +1,109 @@
 import java.util.*;
 import java.io.*;
 
+class Fish implements Comparable<Fish>{
+    int y;
+    int x;
+    int dist;
+    Fish(int y, int x, int dist){
+        this.y = y;
+        this.x = x;
+        this.dist = dist;
+    }
+
+    @Override
+    public int compareTo(Fish o) {
+        if(this.dist == o.dist){
+            if(this.y == o.y){
+                return this.x - o.x;
+            }else return this.y - o.y;
+        }else return this.dist - o.dist;
+    }
+}
+
+class Shark extends Fish{
+    int time;
+    Shark(int y, int x, int time) {
+        super(y,x,0);
+        this.time = time;
+    }
+}
+
 public class p16236 {
     public static int n;
     public static int[][] graph;
-    public static int[][] cnt;
     public static boolean[][] visited;
+    public static ArrayList<Fish> fishList;
+    public static int size = 2;
+    public static int ate = 0;
+
     public static int[] dy = {-1,0,0,1};
     public static int[] dx = {0,-1,1,0};
-    public static int size = 2;
-    public static int[] total = new int[7];
 
-    public static void bfs(int y, int x){
-        Queue<int[]> queue = new LinkedList<>();
-        visited[y][x] = true;
-        graph[y][x] = 0;
-        queue.offer(new int[]{y,x});
-        int fish = 0;
+    public static Shark bfs(Shark s){
+        Queue<Shark> queue = new LinkedList<>();
+        visited[s.y][s.x] = true;
+        queue.offer(s);
         while(!queue.isEmpty()){
-            int[] item = queue.poll();
-            y = item[0];
-            x = item[1];
-            int totalSum = 0;
-            for(int i = 1;i<Math.min(size,7);i++){
-                totalSum += total[i];
-            }
-            if(totalSum==0) break;
+            s = queue.poll();
             for(int i = 0;i<4;i++){
-                int ny = y + dy[i];
-                int nx = x + dx[i];
+                int ny = s.y + dy[i];
+                int nx = s.x + dx[i];
                 if(ny>=0 && nx>=0 && ny<n && nx<n){
-                    if(graph[ny][nx]<=size && !visited[ny][nx]){
+                    if(!visited[ny][nx] && graph[ny][nx]<=size){
                         visited[ny][nx] = true;
-                        cnt[ny][nx] = cnt[y][x] + 1;
-                        queue.offer(new int[]{ny, nx});
-                        if(graph[ny][nx]>0 && graph[ny][nx]<size){
-                            fish++;
-                            total[graph[ny][nx]]--;
-                            graph[ny][nx] = 0;
-                            queue.clear();
-                            visited = new boolean[n][n];
-                            visited[ny][nx] = true;
-                            queue.offer(new int[]{ny, nx});
-                            System.out.println(ny + " " +  nx + " " + cnt[ny][nx]);
-                            if(fish==size) {
-                                size++;
-                                fish=0;
-                            }
-                            break;
+                        queue.offer(new Shark(ny,nx,s.time+1));
+                        if(graph[ny][nx]!=0 && graph[ny][nx]<size){
+                            fishList.add(new Fish(ny,nx,s.time+1));
                         }
                     }
                 }
             }
         }
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                System.out.print(cnt[i][j] + " ");
+        return s;
+    }
+
+    public static void eat(Shark s){
+        while(true){
+            fishList = new ArrayList<>();
+            visited = new boolean[n][n];
+            bfs(s);
+            if(fishList.size()==0){
+                System.out.println(s.time);
+                break;
+            }else{
+                Collections.sort(fishList);
+                Fish f = fishList.get(0);
+                s.y = f.y;
+                s.x = f.x;
+                s.time = f.dist;
+                graph[f.y][f.x] = 0;
+                ate++;
+                if(ate==size){
+                    size++;
+                    ate = 0;
+                }
             }
-            System.out.println();
         }
-        System.out.println("size : " + size + "fish : " + fish);
-        if(size==2 && fish==0) System.out.println(0);
-        else System.out.println(cnt[y][x]);
     }
 
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         graph = new int[n][n];
-        cnt = new int[n][n];
-        visited = new boolean[n][n];
+        Shark shark = new Shark(0,0,0);
         StringTokenizer st;
-        int y = 0;
-        int x = 0;
         for(int i = 0;i<n;i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0;j<n;j++){
                 graph[i][j] = Integer.parseInt(st.nextToken());
                 if(graph[i][j] == 9){
-                    y = i;
-                    x = j;
-                }else if(graph[i][j]>0){
-                    total[graph[i][j]]++;
+                    shark.y = i;
+                    shark.x = j;
+                    graph[i][j] = 0;
                 }
             }
         }
-        bfs(y,x);
+        eat(shark);
     }
 }
