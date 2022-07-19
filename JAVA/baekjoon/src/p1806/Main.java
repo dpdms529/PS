8c0f075e7f83ae59f81ea3c1ef068a327ba07324
YE-EUN <musiclove529@naver.com>
2022-07-19 18:46:35 +0900

    p1806

--- JAVA/baekjoon/src/p1806/Main.java
+++ JAVA/baekjoon/src/p1806/Main.java
@@ -1,7 +1,9 @@
-package Day02.p1806;
+package p1806;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args)throws IOException {
