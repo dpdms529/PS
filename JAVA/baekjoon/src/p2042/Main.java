58adf9a69ffea7e4602e32bf39fc50b1f4307b61
YE-EUN <musiclove529@naver.com>
2022-07-20 21:59:12 +0900

    p2042

--- JAVA/baekjoon/src/p2042/Main.java
+++ JAVA/baekjoon/src/p2042/Main.java
@@ -1,7 +1,9 @@
-package Day03.p2042;
+package p2042;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n,m,k;
