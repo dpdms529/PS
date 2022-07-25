293bbc6acbcef86309664cc4f4f7d2ffb5794349
YE-EUN <musiclove529@naver.com>
2022-07-25 22:13:11 +0900

    p1722

--- JAVA/baekjoon/src/p1722/Main.java
+++ JAVA/baekjoon/src/p1722/Main.java
@@ -1,7 +1,9 @@
-package Day05.p1722;
+package p1722;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static long[] fact;
