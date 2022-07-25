2232c4741205a15707b8942a4fc5e223abb3eaf4
YE-EUN <musiclove529@naver.com>
2022-07-25 22:13:51 +0900

    p2252

--- JAVA/baekjoon/src/p2252/Main.java
+++ JAVA/baekjoon/src/p2252/Main.java
@@ -1,7 +1,12 @@
-package Day06.p2252;
+package p2252;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
