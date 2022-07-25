36e3ec28d2258714a8fc6e81aa9c2f46866414fe
YE-EUN <musiclove529@naver.com>
2022-07-25 22:13:25 +0900

    p1837

--- JAVA/baekjoon/src/p1837/Main.java
+++ JAVA/baekjoon/src/p1837/Main.java
@@ -1,7 +1,11 @@
-package Day05.p1837;
+package p1837;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static ArrayList<Integer> primes = new ArrayList<>();
