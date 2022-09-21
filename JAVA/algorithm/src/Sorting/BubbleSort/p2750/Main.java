ec9ee40077a1013df2fed1a05bc76afdbf02205b
YE-EUN <musiclove529@naver.com>
2022-09-21 18:34:09 +0900

    p2750

--- JAVA/algorithm/src/Sorting/BubbleSort/p2750/Main.java
+++ JAVA/algorithm/src/Sorting/BubbleSort/p2750/Main.java
@@ -1,6 +1,5 @@
 package Sorting.BubbleSort.p2750;
 
-import java.util.*;
 import java.io.*;
 
 public class Main {
@@ -10,6 +9,20 @@ public class Main {
         int arr[] = new int[N];
         for(int i = 0;i<N;i++){
             arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0;i<N-1;i++){
+            for(int j = i+1;j<N;j++){
+                if(arr[i] > arr[j]){
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        for(int i = 0;i<N;i++){
+            System.out.println(arr[i]);
         }
 
     }
