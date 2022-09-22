d64ef945bf45749561ac71af4c1d6b597c4332b4
YE-EUN <musiclove529@naver.com>
2022-09-22 14:39:11 +0900

    p2018

--- JAVA/algorithm/src/DataStructure/TwoPointer/p2018/Main.java
+++ JAVA/algorithm/src/DataStructure/TwoPointer/p2018/Main.java
@@ -1,2 +1,23 @@
-package DataStructure.TwoPointer.p2018;public class Main {
+package DataStructure.TwoPointer.p2018;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        int count = 1;
+        while(end != N){
+            int tmp = 0;
+            if(sum == N) count++;
+            if(sum >= N) tmp -= start++;
+            if(sum <= N) tmp += ++end;
+            sum += tmp;
+        }
+        System.out.println(count);
+    }
 }
