7fb674eda1ea952c1e3c63d9f1d7e109f58f35d2
YE-EUN <musiclove529@naver.com>
2022-09-23 18:49:02 +0900

    p11003

--- JAVA/algorithm/src/DataStructure/SlidingWindow/p11003/Main.java
+++ JAVA/algorithm/src/DataStructure/SlidingWindow/p11003/Main.java
@@ -1,2 +1,37 @@
-package DataStructure.SlidingWindow.p11003;public class Main {
+package DataStructure.SlidingWindow.p11003;
+
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int index;
+    int value;
+    public Node(int index, int value){
+        this.index = index;
+        this.value = value;
+    }
+}
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i<N;i++){
+            int current = Integer.parseInt(st.nextToken());
+            while(!deque.isEmpty() && deque.getLast().value > current){
+                deque.removeLast();
+            }
+            deque.addLast(new Node(i, current));
+            if(deque.getFirst().index <= i - L){
+                deque.removeFirst();
+            }
+            sb.append(deque.getFirst().value + " ");
+        }
+        System.out.println(sb);
+    }
 }
