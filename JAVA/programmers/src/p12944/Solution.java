3794e6448a875aba6262145fd689c21b9cea5bf5
YE-EUN <musiclove529@naver.com>
2022-12-07 23:36:08 +0900

    p12944

--- JAVA/programmers/src/p12944/Solution.java
+++ JAVA/programmers/src/p12944/Solution.java
@@ -3,7 +3,12 @@ package p12944;
 import java.util.Arrays;
 
 class Solution {
-    public double solution(int[] arr) {
+    public static double solution(int[] arr) {
         return Arrays.stream(arr).average().getAsDouble();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        System.out.println(Solution.solution(arr));
     }
 }
