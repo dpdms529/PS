0dc2da6a7da0f8deaecc0298a769215a25070e39
YE-EUN <musiclove529@naver.com>
2022-12-09 20:25:48 +0900

    p120804

---
+++ programmers/src/p120804/Solution.java
@@ -0,0 +1,13 @@
+package p120804;
+
+class Solution {
+    public static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 27, num2 = 19;
+        System.out.println(solution(num1, num2));
+
+    }
+}
