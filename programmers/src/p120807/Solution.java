5dea6e20e6524c0b90d097cb665210b38c71c904
YE-EUN <musiclove529@naver.com>
2022-12-09 20:26:15 +0900

    p120807

---
+++ programmers/src/p120807/Solution.java
@@ -0,0 +1,12 @@
+package p120807;
+
+class Solution {
+    public static int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 2, num2 = 3;
+        System.out.println(solution(num1, num2));
+    }
+}
