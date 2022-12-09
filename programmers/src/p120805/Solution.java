ee871d7959dc28f68e41f4b2245c6bca9d42f162
YE-EUN <musiclove529@naver.com>
2022-12-09 20:25:58 +0900

    p120805

---
+++ programmers/src/p120805/Solution.java
@@ -0,0 +1,13 @@
+package p120805;
+
+class Solution {
+    public static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 5;
+        System.out.println(solution(num1, num2));
+
+    }
+}
