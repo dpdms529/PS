79f7517afc11349ace3fe9d303720b7d81845076
YE-EUN <musiclove529@naver.com>
2022-12-09 20:28:58 +0900

    p120810

---
+++ programmers/src/p120810/Solution.java
@@ -0,0 +1,13 @@
+package p120810;
+
+class Solution {
+    public static int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 2;
+        System.out.println(solution(num1, num2));
+
+    }
+}
