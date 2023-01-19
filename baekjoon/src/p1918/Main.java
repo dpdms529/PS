95f6d4ded328901c65321091678905fc67b9d02d
YE-EUN <musiclove529@naver.com>
2023-01-20 01:16:31 +0900

    p1918

--- baekjoon/src/p1918/Main.java
+++ baekjoon/src/p1918/Main.java
@@ -6,12 +6,53 @@ import java.io.*;
 public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String exp = br.readLine();
+
+        String expr = br.readLine();
+
+        Map<Character, Integer> priority = Map.of('(', 1, '+', 2, '-', 2, '*', 3, '/', 3, ')', 4);
+
+
         Stack<Character> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<exp.length();i++){
-            char c = exp.charAt(i);
 
+        for(int i = 0;i<expr.length();i++){
+            char c = expr.charAt(i);
+
+            if(c >= 'A' && c <= 'Z'){
+                sb.append(c);
+            }else{
+                while(c != '(' && !stack.isEmpty() && priority.get(stack.peek()) >= priority.get(c)){
+                    if(stack.peek() == ')'){
+                        stack.pop();
+
+                        while(stack.peek() != '('){
+                            sb.append(stack.pop());
+                        }
+
+                        stack.pop();
+                    }else{
+                        sb.append(stack.pop());
+                    }
+                }
+
+                stack.push(c);
+            }
         }
+
+        while(!stack.isEmpty()){
+            if(stack.peek() == ')'){
+                stack.pop();
+
+                while(stack.peek() != '('){
+                    sb.append(stack.pop());
+                }
+
+                stack.pop();
+            }else{
+                sb.append(stack.pop());
+            }
+        }
+
+        System.out.println(sb);
     }
 }
