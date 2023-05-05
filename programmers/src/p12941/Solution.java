def918205d65db7162bbf6c7ef2f1fb35ad10778
YE-EUN <musiclove529@naver.com>
2023-05-05 21:22:20 +0900

    p12941

---
+++ programmers/src/p12941/Solution.java
@@ -0,0 +1,26 @@
+package p12941;
+
+import java.util.Arrays;
+
+/**
+ * 최솟값 만들기
+ * Level 2
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12941
+ */
+public class Solution {
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int len = A.length;
+
+        for(int i = 0;i<len;i++){
+            answer += A[i] * B[len - 1 - i];
+        }
+
+        return answer;
+    }
+}
