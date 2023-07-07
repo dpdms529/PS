9c46f52a2cc921f9fe2749c4d656c11eb6520e9c
YE-EUN <musiclove529@naver.com>
2023-07-07 23:43:20 +0900

    42895

--- programmers/src/p42895/Solution.java
+++ programmers/src/p42895/Solution.java
@@ -1,2 +1,11 @@
-package p42895;public class Solution {
+package p42895;
+
+class Solution {
+    public int solution(int N, int number) {
+        int answer = 0;
+
+        int[] arr = new int[number+1];
+
+        return answer;
+    }
 }
