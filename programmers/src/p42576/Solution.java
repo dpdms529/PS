6072b8e25aa1a3d1a4ef9d63186bafc5f5127129
YE-EUN <musiclove529@naver.com>
2023-07-07 23:42:58 +0900

    p42576

--- programmers/src/p42576/Solution.java
+++ programmers/src/p42576/Solution.java
@@ -1,2 +1,27 @@
-package p42576;public class Solution {
+package p42576;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion) {
+            int cnt = map.get(c);
+
+            if(cnt - 1 == 0) {
+                map.remove(c);
+            }else {
+                map.put(c, cnt - 1);
+            }
+        }
+
+        return map.keySet()
+                .iterator()
+                .next();
+    }
 }
