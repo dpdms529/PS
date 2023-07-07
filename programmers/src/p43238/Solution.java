1b978c8c03846efee35a8b295315966934292907
YE-EUN <musiclove529@naver.com>
2023-07-07 23:44:04 +0900

    p43238

--- programmers/src/p43238/Solution.java
+++ programmers/src/p43238/Solution.java
@@ -1,2 +1,32 @@
-package p43238;public class Solution {
+package p43238;
+
+class Solution {
+    static final long MAX_PEOPLE = 1000000000;
+    static final long MAX_TIME = 1000000000;
+
+    public long solution(int n, int[] times) {
+        long answer = 0;
+
+        long left = 1;
+        long right = MAX_PEOPLE * MAX_TIME;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            long sum = 0;
+            for (int time : times) {
+                sum += mid / time;
+            }
+
+            if (sum >= n) {
+                answer = mid;
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+
+
+        return answer;
+    }
 }
