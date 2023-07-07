de1e0a84d0e612daa31811d20e53bf15534bea67
YE-EUN <musiclove529@naver.com>
2023-07-07 23:43:33 +0900

    p43162

--- programmers/src/p43162/Solution.java
+++ programmers/src/p43162/Solution.java
@@ -1,2 +1,51 @@
-package p43162;public class Solution {
+package p43162;
+
+import java.util.*;
+
+class Solution {
+    static List<Integer>[] network;
+    static boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+
+        network = new ArrayList[n];
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            network[i] = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                if(i==j) continue;
+
+                if(computers[i][j] == 1) {
+                    network[i].add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                bfs(i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int next : network[cur]) {
+                if(!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
 }
