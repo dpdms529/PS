e061185bd7faf01e7c4b77851dcf956ff5ff8b25
YE-EUN <musiclove529@naver.com>
2023-07-07 09:45:15 +0900

    p42747

--- programmers/src/p42747/Solution.java
+++ programmers/src/p42747/Solution.java
@@ -1,2 +1,28 @@
-package p42747;public class Solution {
+package p42747;
+
+import java.util.Arrays;
+
+class Solution {
+    public static int solution(int[] citations) {
+        int h = 0;
+
+        Arrays.sort(citations);
+        int maxCitation = citations[citations.length-1];
+
+        int start = 0;
+        for(int i = 0;i<maxCitation;i++) {
+            for(int j = start;j<citations.length;j++) {
+                if(citations[j] >= i) {
+                    if(citations.length - j >= i) {
+                        h = i;
+                    }
+
+                    start = j;
+                    break;
+                }
+            }
+        }
+
+        return h;
+    }
 }
