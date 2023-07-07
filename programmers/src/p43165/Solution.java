ce7fd04bea055f8d6a8f8e9e46a40e347bd04ba6
YE-EUN <musiclove529@naver.com>
2023-07-07 23:43:51 +0900

    p43165

--- programmers/src/p43165/Solution.java
+++ programmers/src/p43165/Solution.java
@@ -1,2 +1,23 @@
-package p43165;public class Solution {
+package p43165;
+
+class Solution {
+    static int cnt = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+
+        return cnt;
+    }
+
+    public void dfs(int[] numbers, int target, int sum, int depth) {
+        if(depth == numbers.length) {
+            if(sum == target) {
+                cnt++;
+            }
+        }else {
+            sum += numbers[depth];
+            dfs(numbers, target, sum, depth+1);
+            sum -= numbers[depth] * 2;
+            dfs(numbers, target, sum, depth+1);
+        }
+    }
 }
