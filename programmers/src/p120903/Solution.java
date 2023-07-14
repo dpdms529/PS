6120dd5aeb0b089908d283708e97a7b11fe6c43e
YE-EUN <musiclove529@naver.com>
2023-07-14 16:39:17 +0900

    p120903

--- programmers/src/p120903/Solution.java
+++ programmers/src/p120903/Solution.java
@@ -1,2 +1,24 @@
-package p120903;public class Solution {
+package p120903;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        int start = 0;
+        for (String s : s1) {
+            for(int i = start;i< s2.length;i++) {
+                if(s.equals(s2[i])) {
+                    answer++;
+                    start = i + 1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
 }
