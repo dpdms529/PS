82fd1689269c1afb065f7140b919d7751b153c7a
YE-EUN <musiclove529@naver.com>
2023-07-14 16:40:03 +0900

    p181891

--- programmers/src/p181891/Solution.java
+++ programmers/src/p181891/Solution.java
@@ -1,2 +1,16 @@
-package p181891;public class Solution {
+package p181891;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+
+        for (int i = 0; i < n; i++) {
+            answer[num_list.length-n+i] = num_list[i];
+        }
+        for (int i = n; i < num_list.length; i++) {
+            answer[i-n] = num_list[i];
+        }
+
+        return answer;
+    }
 }
