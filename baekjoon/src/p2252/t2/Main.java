86f61ba1cae801459bfb79352c7213bd14ff9f9f
YE-EUN <musiclove529@naver.com>
2023-08-17 22:06:04 +0900

    p2252

---
+++ baekjoon/src/p2252/t2/Main.java
@@ -0,0 +1,60 @@
+package p2252.t2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static ArrayList<Integer>[] graph;
+    static int[] indegree;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N+1];
+        indegree = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            indegree[b]++;
+        }
+
+        topologicalSort();
+        System.out.println(sb);
+    }
+
+    static void topologicalSort() {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            if(indegree[i] == 0) queue.offer(i);
+        }
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+
+            for(int next : graph[node]) {
+                indegree[next]--;
+                if(indegree[next] == 0) queue.offer(next);
+            }
+
+            sb.append(node + " ");
+        }
+    }
+}
