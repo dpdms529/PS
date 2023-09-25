42eac58e80274cd265fed003aaa5ab2bebff6ff4
YE-EUN <musiclove529@naver.com>
2023-09-25 10:34:53 +0900

    p2143

--- baekjoon/src/p2143/t2/Main.java
+++ baekjoon/src/p2143/t2/Main.java
@@ -1,2 +1,29 @@
-package p2143.t2;public class Main {
+package p2143.t2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        int[] B = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+    }
 }
