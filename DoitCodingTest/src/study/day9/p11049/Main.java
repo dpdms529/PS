a77ab494f51eb52c13dfac10e6395deef83d1d01
YE-EUN <musiclove529@naver.com>
2023-09-25 10:30:28 +0900

    day9

--- DoitCodingTest/src/study/day9/p11049/Main.java
+++ DoitCodingTest/src/study/day9/p11049/Main.java
@@ -1,2 +1,20 @@
-package study.day9.p11049;public class Main {
+package study.day9.p11049;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+        }
+
+    }
 }
--- DoitCodingTest/src/study/day9/p9252/Main.java
+++ DoitCodingTest/src/study/day9/p9252/Main.java
@@ -1,2 +1,16 @@
-package study.day9.p9252;public class Main {
+package study.day9.p9252;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s1 = br.readLine().split("");
+        String[] s2 = br.readLine().split("");
+        
+
+
+    }
 }
