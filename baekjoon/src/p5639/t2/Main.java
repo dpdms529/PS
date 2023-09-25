c6de578e4b87e17887298f4dbe1624004860ea1a
YE-EUN <musiclove529@naver.com>
2023-09-25 13:43:57 +0900

    p5639

--- baekjoon/src/p5639/t2/Main.java
+++ baekjoon/src/p5639/t2/Main.java
@@ -1,2 +1,33 @@
-package p5639.t2;public class Main {
+package p5639.t2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] graph = new int[20000];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        int idx = 1;
+        int node = Integer.parseInt(br.readLine());
+        graph[idx] = node;
+
+        while((s = br.readLine()) != null) {
+            node = Integer.parseInt(s);
+            if(node < graph[idx]) {
+                idx *= 2;
+                graph[idx] = node;
+            }else if(node > graph[idx]) {
+                int parent = idx / 2;
+                while(parent > 0 && node > graph[parent]) {
+                    if(graph[parent*2+1] == 0) {
+                        idx = parent*2+1;
+                    }
+
+                }
+            }
+
+        }
+    }
 }
