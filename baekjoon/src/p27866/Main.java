df625cf04f12446fc095f1afe6fcccfa2e97b300
YE-EUN <musiclove529@naver.com>
2024-05-23 22:52:38 +0900

    p27866

--- baekjoon/src/p27866/Main.java
+++ baekjoon/src/p27866/Main.java
@@ -1,2 +1,12 @@
-package p27866;public class Main {
+package p27866;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int i = sc.nextInt() - 1;
+        System.out.println(s.charAt(i));
+    }
 }
