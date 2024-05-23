3a9e75ae943f2cbd3090b8ea1dadd441fae28719
YE-EUN <musiclove529@naver.com>
2024-05-23 22:52:12 +0900

    p18110

--- baekjoon/src/p18110/Main.java
+++ baekjoon/src/p18110/Main.java
@@ -1,2 +1,33 @@
-package p18110;public class Main {
+package p18110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] difficulty = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            difficulty[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(difficulty);
+
+        int remove = Math.round((float)n / 100 * 15);
+        int start = remove;
+        int end = n - remove;
+        int sum = 0;
+
+        for (int i = start; i < end; i++) {
+            sum += difficulty[i];
+        }
+
+        int result = Math.round((float)sum / (end - start));
+        System.out.println(result);
+    }
 }
